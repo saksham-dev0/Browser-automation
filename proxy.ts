bf1b0005db8895b4909ff5f7603b6f160136f45a
saksham-dev0 <sakshamsharmaofficial7@gmail.com>
2026-08-20 00:14:10 +0530

    FEAT: Organization setup completed

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isProtectedRoute = createRouteMatcher(["/test(.*)"])
+const isProtectedRoute = createRouteMatcher(["/"])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
